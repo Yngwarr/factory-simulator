bd1bc4730b4726ddb14257fb378e22e06d1748cf
Yngvarr <k.yngvarr@gmail.com>
2024-09-24 21:36:29 +0200

    apply grid

--- src/pages/Factory/index.tsx
+++ src/pages/Factory/index.tsx
@@ -1,6 +1,9 @@
 export function FactoryPage() {
 	return (
-		<div id="factory" className="w-full h-full grid grid-cols-2">
+		<div id="factory" className="w-full h-full grid grid-cols-factory grid-rows-factory">
+			<div className="col-span-2">heading</div>
+			<div>resources</div>
+			<div>chain</div>
 		</div>
 	);
 }
